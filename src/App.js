82bf6583b340b1651242e8b822ff3921c1fe8497
zorgonide <ammar1998@gmail.com>
2022-10-01 12:57:12 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="display-1">
+            <div className="col">
+              Transport application
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
