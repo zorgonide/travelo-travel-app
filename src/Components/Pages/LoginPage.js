a02fc47407ceec04dee853e38f5f3e6228cf5625
zorgonide <ammar1998@gmail.com>
2022-10-09 19:30:01 +0100

    Login page

--- src/Components/Pages/LoginPage.js
+++ src/Components/Pages/LoginPage.js
@@ -1,18 +1,86 @@
-import React from 'react'
-import { useState, createContext, useContext } from "react";
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import LogIn from "../Images/login1.svg";
+import { Link } from "react-router-dom";
+import * as Yup from "yup";
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string().required("Required"),
+});
+
 function LoginPage() {
-  // const {loggedIn, setLoggedIn} = useContext(UserContext);
   return (
-    <div>
-      Login page
-      {/* <button onClick={()=> setLoggedIn(true)}>
-        Log in
-      </button>
-      <button onClick={()=> setLoggedIn(false)}>
-        Log out
-      </button> */}
+    <div className="container-fluid">
+      <div className="row justify-content-center align-items-center">
+        <div className="col-sm-12 text-center">
+          <div className="card">
+            <div className="card-body">
+              <p className="card-title display-5 gray">Log In</p>
+              <div className="img">
+                <img
+                  src={LogIn}
+                  width="330"
+                  height="330"
+                  className=""
+                  alt="login"
+                />
+              </div>
+              <Formik
+                initialValues={{
+                  password: "",
+                  email: "",
+                }}
+                validationSchema={LoginSchema}
+                onSubmit={async (values) => {
+                  await new Promise((r) => setTimeout(r, 500));
+                  alert(JSON.stringify(values, null, 2));
+                }}
+              >
+                {({ errors, touched }) => (
+                  <Form>
+                    <div className="form-floating mb-3">
+                      <Field
+                        className="form-control"
+                        id="email"
+                        name="email"
+                        placeholder="example@email.com"
+                      />
+                      <label htmlFor="email">Email</label>
+                      <div className="error">
+                        <ErrorMessage name="email" />
+                      </div>
+                    </div>
+                    <div className="form-floating mb-3">
+                      <Field
+                        className="form-control"
+                        id="password"
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                      />
+                      <label htmlFor="password">Password</label>
+                      <div className="error">
+                        <ErrorMessage name="password" />
+                      </div>
+                    </div>
+                    <div className="d-grid gap-2">
+                      <button type="submit" className="button button1 mb-3">
+                        Log in
+                      </button>
+                    </div>
+                  </Form>
+                )}
+              </Formik>
+            </div>
+            <div className="card-footer text-muted link">
+              Not registered? <Link to="/register">Sign Up</Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- src/Components/Pages/Main.js
+++ src/Components/Pages/Main.js
@@ -7,21 +7,23 @@ import RegisterPage from "./RegisterPage";
 
 function Main() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/" element={<ProtectedRoute />}>
-        <Route path="/" element={<HomePage />} />
-      </Route>
-      <Route
-        path="*"
-        element={
-          <div>
-            <p>Page not found!</p>
-          </div>
-        }
-      />
-    </Routes>
+    <div className="app">
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<ProtectedRoute />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <div>
+              <p>Page not found!</p>
+            </div>
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
