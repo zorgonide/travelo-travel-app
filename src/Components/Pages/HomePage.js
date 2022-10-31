e4dd268423471a20b7f7de1e14610b9cc7bbc284
zorgonide <ammar1998@gmail.com>
2022-10-31 12:28:15 +0000

    OPerator in progress

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -1,22 +1,13 @@
-import React from 'react'
-import CustomerHomePage from './CustomerHomePage'
-import ManagerHomePage from './ManagerHomePage'
-import OperatorHomePage from './OperatorHomePage'
+import React from "react";
+import CustomerHomePage from "./CustomerHomePage";
+import ManagerHomePage from "./ManagerHomePage";
+import OperatorHomePage from "./OperatorHomePage";
 
 function HomePage() {
-  let user = { type: "customer"}
-  if (user.type === "customer")
-    return (
-      <CustomerHomePage/>
-    )
-  else if (user.type === "manager")
-    return (
-      <ManagerHomePage/>
-    )
-  else if (user.type === "operator")
-    return (
-      <OperatorHomePage/>
-    )
+  let user = { type: "operator" };
+  if (user.type === "customer") return <CustomerHomePage />;
+  else if (user.type === "manager") return <ManagerHomePage />;
+  else if (user.type === "operator") return <OperatorHomePage />;
 }
 
-export default HomePage
+export default HomePage;
--- src/Components/Pages/OperatorHomePage.js
+++ src/Components/Pages/OperatorHomePage.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLocationCrosshairs,
+  faWrench,
+  faBug,
+} from "@fortawesome/free-solid-svg-icons";
 
 function OperatorHomePage() {
+  const navigate = useNavigate();
   return (
-    <div>OperatorHomePage</div>
-  )
+    <div className="container-fluid">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-5 text-center">
+            <div className="card my-3">
+              <div className="card-body">
+                <p className="card-title display-6 gray">Select Action</p>
+                <hr />
+                <div className="d-grid gap-2">
+                  <button
+                    onClick={() => navigate("/return")}
+                    className="button button1 mb-3"
+                  >
+                    <FontAwesomeIcon icon={faLocationCrosshairs} /> Track
+                  </button>
+                </div>
+                <div className="d-grid gap-2">
+                  <button
+                    onClick={() => navigate("/rent")}
+                    className="button button1 mb-3"
+                  >
+                    <FontAwesomeIcon icon={faWrench} /> Action
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default OperatorHomePage
+export default OperatorHomePage;
